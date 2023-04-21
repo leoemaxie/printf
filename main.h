a8a0ed084aeb013544477e4abe1eafe3e827cda1
leoemaxie <leoemaxie@gmail.com>
2023-04-21 10:46:50 +0100

    Update README.md

--- main.h
+++ main.h
@@ -1,11 +1,11 @@
-#ifndef _IO_H_
-#define _IO_H_
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
+/* includes */
 #include <stdarg.h>
 
 /* Declarations */
-void _printf(const char *fmt, ...);
+int _printf(const char *fmt, ...);
 void _putchar(char c);
-void _puts(char *c);
 
 #endif
