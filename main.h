590c83ff0c3122fca5de2996b021a7921d249975
Ojikutu Mary <maryojiks@yahoo.com>
2023-04-24 14:42:31 -0700

    second commit

--- main.h
+++ main.h
@@ -1,11 +1,13 @@
-#ifndef _IO_H_
-#define _IO_H_
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdarg.h>
 
 /* Declarations */
-void _printf(const char *fmt, ...);
+int _printf(const char *format, ...);
 void _putchar(char c);
 void _puts(char *c);
-
+int print_string(char *str);
+void print_char(va_list *ap);
+void print_str(va_list *ap);
 #endif
